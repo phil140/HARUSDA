aed23bc44c94324cfb25a6edc3643e0784d97719
phil140 <mm@steveterry.com>
2015-06-11 10:05:26 -0400

    Initial
    Initial file for gitthub repository

---
+++ run_analysis.R
@@ -0,0 +1,12 @@
+# File: run_analysis.R
+# Date: 6/11/2015
+# Author: Phil Sophity
+# Description:
+#    1. Merges the training and the test sets to create one data set.
+#    2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#    3. Uses descriptive activity names to name the activities in the data set
+#    4. Appropriately labels the data set with descriptive variable names. 
+# See also:
+#    1. README.md
+#    2. PhilsCodeBook.txt
+
